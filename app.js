5a477bc1da6a0ef3fa5111ce0e76296ca720869d
EstherMx <esmaxi21@gmail.com>
2018-06-14 23:24:32 -0400

    CRUD operations

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-var express    = require("express"),
-	app 	   = express(),
-	mongoose   = require("mongoose"),
-	bodyParser = require("body-parser");
+var express = require("express"),
+	app = express(),
+	mongoose = require("mongoose"),
+	bodyParser = require("body-parser"),
+	methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public")); //for styling any Express app
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method")); //will change the current route to another one
 
 
 //Mongoose model 
@@ -56,8 +58,55 @@ app.post("/blogs",function(req,res){
 			//then, redirect to the index
 			res.redirect("/blogs");
 		}
+	});
+});
+
+//SHOW ROUTE
+app.get("/blogs/:id", function(req,res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		}else{
+			res.render("show", {blog: foundBlog});
+		}
+	});
+});
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req,res){
+	Blog.findById(req.params.id, function(err,foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		}else{
+			res.render("edit", {blog:foundBlog});
+		}
+	});
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+	// Blog.findByIdAndUpdate(id, newData, callback)
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+		if(err){
+			res.redirect("/blogs");
+		}else{
+			res.redirect("/blogs/" + req.params.id);
+		}
 	})
 })
+
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req,res){
+	//destroy blog
+	Blog.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/blogs");
+	//redirect somewhere
+		}else{
+			res.redirect("/blogs");
+		}
+	});
+});
 
 var port = process.env.PORT || 3000;
 app.listen(3000, function(){
